24585df1c3f1041e452c5ad24aceefae5f491a55
mmollica <mmollica@spsu.edu>
2014-04-06 15:04:10 -0400

    Base stuff we need

---
+++ classes/Config.php
@@ -0,0 +1,27 @@
+<?php
+//CODED BY: Travis Harrell
+//Config class for configuration settings
+
+class Config
+{
+	public static function get($path=null)
+	{
+		if($path)
+		{
+			$config = $GLOBALS['config'];
+			$path = explode('/', $path);
+
+			foreach ($path as $bit)
+		    {
+				if(isset($config[$bit]))
+				{
+					$config = $config[$bit];
+				}
+			}
+
+			return $config;
+		}
+
+		return false;
+	}
+}
